6efb836c63c44438e10ba7a65edafd5440745fa7
yungshun317 <seraph317@gmail.com>
2023-05-29 17:52:08 +0800

    [FEAT] Change color and shape depending on `elapsedTime`

--- src/script.js
+++ src/script.js
@@ -7,6 +7,15 @@ import fShader from "./shaders/fragment.glsl";
 // [1] Scene
 const scene = new THREE.Scene();
 
+// Cursor
+const cursor = {
+    x: 0, y: 0
+};
+window.addEventListener("mouseover", (e) => {
+   cursor.x = e.clientX / window.innerWidth;
+   cursor.y = e.clientY / window.innerHeight;
+});
+
 // Responsive
 window.addEventListener("resize", () => {
     // Update size
@@ -26,8 +35,16 @@ window.addEventListener("resize", () => {
 const geometry = new THREE.PlaneGeometry(0.75, 0.75, 64, 64);
 console.log(geometry);
 const material = new THREE.RawShaderMaterial({
+    side: THREE.DoubleSide,
     vertexShader: vShader,
-    fragmentShader: fShader
+    fragmentShader: fShader,
+    uniforms: {
+        u_amplitude: { value: 12.0 },
+        u_time: { value: 0.0 },
+        u_color: { value: new THREE.Color("purple") },
+        u_timecolor: { value: 0 },
+        u_cursorcolor: { value: new THREE.Vector2(cursor.x, cursor.y) }
+    }
 });
 console.log(material);
 const mesh = new THREE.Mesh(geometry, material);
@@ -43,6 +60,7 @@ geometry.setAttribute(
     "a_modulus",
     new THREE.BufferAttribute(newAttributeArray, 1)
 );
+console.log(newAttributeArray);
 
 // [3] Camera
 const aspect = {
@@ -50,7 +68,7 @@ const aspect = {
     height: window.innerHeight
 }
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.z = 3;
+camera.position.z = 1;
 scene.add(camera);
 
 // [4] Renderer
@@ -68,6 +86,16 @@ const clock = new THREE.Clock();
 const animate = () => {
     // GetElapsedTime
     const elapsedTime = clock.getElapsedTime();
+
+    // Update `u_time`
+    material.uniforms.u_time.value = elapsedTime;
+
+    // Update `u_timecolor`
+    material.uniforms.u_timecolor.value = elapsedTime;
+
+    // Update `u_cursorcolor`
+    material.uniforms.u_cursorcolor.value.x = cursor.x;
+    material.uniforms.u_cursorcolor.value.y = cursor.y;
 
     // Update orbitControls
     orbitControls.update();
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,15 +1,13 @@
 precision mediump float;
-
-varying vec3 v_position;
-varying vec2 v_uv;
-varying float v_a_modulus;
+uniform vec3 u_color;
+uniform float u_timecolor;
+uniform vec2 u_cursorcolor;
 
 void main() {
-    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0).grba;
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    // gl_FragColor = vec4(v_position, 1.0);
-    // gl_FragColor = vec4(v_uv, 1.0, 1.0);
-    // gl_FragColor = vec4(1.0, v_uv.x, 1.0, 1.0);
+    gl_FragColor = vec4(u_color, 1.0);
 
-    gl_FragColor = vec4(v_a_modulus, 1.0, 1.0, 1.0);
+    // Change color depending on `elapsedTime`
+    gl_FragColor.r = 1.0 + sin(u_timecolor);
+    gl_FragColor.g = cos(u_timecolor);
+    gl_FragColor.b = -sin(u_timecolor);
 }
