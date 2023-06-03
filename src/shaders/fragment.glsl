d0734711052cbb2042552453755091e285e8fd42
yungshun317 <seraph317@gmail.com>
2023-06-03 19:11:39 +0800

    [FEAT] Create a gradient with the `clamp` function

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -20,6 +20,13 @@ void main() {
     // gl_FragColor = vec4(copy_uvx, v_uv.y, 1.0, 1.0);
 
     // `mix`
-    vec3 mixing = mix(vec3(0.0, 0.0, 1.0), vec3(0.0, 1.0, 0.0), v_uv.x);
-    gl_FragColor = vec4(mixing, 1.0);
+    // vec3 mixing = mix(vec3(0.0, 0.0, 1.0), vec3(0.0, 1.0, 0.0), v_uv.x);
+    // gl_FragColor = vec4(mixing, 1.0);
+
+    // `clamp`
+    vec3 color = vec3(1.0, 1.0, 1.0);
+    color.r = clamp(v_uv.y, 0.0, 1.0);
+    color.g = clamp(v_uv.x, 0.0, 1.0);
+    color.b = clamp(v_uv.x, 0.0, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
