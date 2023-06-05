21b947497b2d9de2ff8fc127d4bf9d4712d38bbb
yungshun317 <seraph317@gmail.com>
2023-06-05 19:18:19 +0800

    [FEAT] Create colored squares with the `step` function

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
 
 varying vec2 v_uv;
+varying vec3 v_position;
 
 void main() {
     /*
@@ -18,15 +19,21 @@ void main() {
     // `step`
     // float copy_uvx = step(v_uv.x, 0.5);
     // gl_FragColor = vec4(copy_uvx, v_uv.y, 1.0, 1.0);
+    vec3 color = vec3(1.0, 1.0, 1.0);
+    color.r = step(0.0, v_position.x);
+    color.g = step(0.0, v_position.y);
+    gl_FragColor = vec4(color, 1.0);
 
     // `mix`
     // vec3 mixing = mix(vec3(0.0, 0.0, 1.0), vec3(0.0, 1.0, 0.0), v_uv.x);
     // gl_FragColor = vec4(mixing, 1.0);
 
     // `clamp`
+    /*
     vec3 color = vec3(1.0, 1.0, 1.0);
     color.r = clamp(v_uv.y, 0.0, 1.0);
     color.g = clamp(v_uv.x, 0.0, 1.0);
     color.b = clamp(v_uv.x, 0.0, 1.0);
     gl_FragColor = vec4(color, 1.0);
+    */
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -6,6 +6,7 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 v_uv;
+varying vec3 v_position;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -14,4 +15,5 @@ void main() {
 
     gl_Position = projectionPosition;
     v_uv = uv;
+    v_position = position;
 }
